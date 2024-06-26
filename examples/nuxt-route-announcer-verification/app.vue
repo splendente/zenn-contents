d14e87dddadbbd5ae53c8912a18b868c21e148bb
Hikaru Kobayashi <hikaru.kobayashi0719+devlop@gmail.com>
2024-06-27 00:06:58 +0900

    :rocket: reset props in NuxtRouteAnnouncer

--- examples/nuxt-route-announcer-verification/app.vue
+++ examples/nuxt-route-announcer-verification/app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- MEMO:propsの値を変更することで読み上げの内容と通知のタイミングを変更することができます。 -->
-    <NuxtRouteAnnouncer :atomic="false" politeness="off">
+    <NuxtRouteAnnouncer>
       <template #default="{ message }">
         <p>ページタイトルが<span>{{ message }}</span>に変更されました。</p>
       </template>
