ad55ce234dd903f9ddead1549ffe483898ae8bc8
Hikaru Kobayashi <hikaru.kobayashi0719+devlop@gmail.com>
2024-06-28 00:16:23 +0900

    :rocket: call emit function

--- examples/6d9f404163f259/pages/index.vue
+++ examples/6d9f404163f259/pages/index.vue
@@ -1,6 +1,15 @@
+<script setup lang="ts">
+/*
+ * ボタンがクリックされたらアラートを表示する
+ */
+const showAlert = () => {
+  window.alert("buttonタグがクリックされました");
+};
+</script>
+
 <template>
   <div>
-    <Button text="buttonタグ" element="button" />
+    <Button text="buttonタグ" element="button" @on-click="showAlert" />
     <Button text="aタグ" element="anchor-link" href="#" target="_blank" />
     <Button text="NuxtLinkコンポーネント" element="nuxt-link" to="#" />
   </div>
